7f5dfc7113333a2a379270d8b511e447d5850841
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2018-06-05 01:43:32 +0100

    attempt to add pass the variable to the content script

--- upwork_job_2/background.js
+++ upwork_job_2/background.js
@@ -9,17 +9,22 @@ chrome.contextMenus.onClicked.addListener(function(info,tab) {
   var action_url = "https://selling.autotrader.co.uk";
   chrome.tabs.query({currentWindow: true}, function(callback){
     var value = callback.length;
-    console.log(value);
-    chrome.tabs.create({url: action_url, index: value + 1});
-    chrome.tabs.executeScript(value + 1 , {code:"var selectionText = info.selectionText", file:"script.js", runAt:"document_end"}, function(callback){
-      console.log("over");
-      chrome.tabs.remove(value + 1);
+    chrome.tabs.create({url: action_url, index: value});
+    chrome.tabs.executeScript(value+1, {file:'script.js'},function(callback) {
+        console.log('envia');
+        chrome.tabs.sendMessage(value+1, info.selectionText);
+        console.log('ja enviou');
+      });
     });
-  });
+    //chrome.tabs.remove(value);
 
-
-  alert(info.selectionText);
-
+/*
   var action_url_2 = "https://www.copart.co.uk/"; // to be added after.
-  chrome.tabs.create({url: action_url_2})
+  chrome.tabs.create({url: action_url_2});
+  */
+});
+
+
+chrome.tabs.onCreated.addListener(function(tab){
+    alert("new tab "+tab.id);
 });
--- upwork_job_2/script.js
+++ upwork_job_2/script.js
@@ -1,6 +1,11 @@
 
-//do work
-console.log('teste');
-var teste = document.getElementById("mileage").value = "200000";
-console.log(teste);
-console.log('passou');
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    // Handle message.
+    // In this example, message === 'whatever value; String, object, whatever'
+    console.log(message);
+    var teste = document.getElementById("mileage").value = "200000";
+    console.log(teste);
+    console.log('passou');
+
+});
